29f87c1ce840ccc2f68fb0d0b75204e5b27aebed
FatihG34 <fthgnydn34@gmail.com>
2022-10-01 22:58:27 +0300

    Django Debug Tool Bar added

--- main/urls.py
+++ main/urls.py
@@ -44,4 +44,7 @@ urlpatterns = [
          name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc",
          cache_timeout=0), name="schemaredoc"),
+
+    # url pattern for Django Debug Tool Bar
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
